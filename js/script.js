afcc17cdef14a2cffe9393bb9cf3bfd61b78b82f
Francesco <francescofogliajj@gmail.com>
2020-12-18 23:26:26 +0100

    Commit #2

--- js/script.js
+++ js/script.js
@@ -4,21 +4,44 @@ var app = new Vue ({
 
   data: {
     movies: [],
-    posterPath : "https://image.tmdb.org/t/p/w220_and_h330_face/"
+    posterPath: "https://image.tmdb.org/t/p/w220_and_h330_face/",
+    userInput: ""
   },
 
   methods: {
 
+    search() {
+
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: "149f6f231b74bbad3f1401daa78a4cce",
+            query: this.userInput,
+            language: "it-IT"
+          }
+        })
+        .then( result => {
+          this.movies = result.data.results;
+      });
+
+    }
+
   },
 
-  mounted: function() {
-
-    axios
-      .get("https://api.themoviedb.org/3/search/movie?api_key=149f6f231b74bbad3f1401daa78a4cce&query=ritorno+al+futuro")
-      .then( result => {
-        this.movies = result.data.results;
-    });
-
-  }
+  // mounted: function() {
+  //
+  //   axios
+  //     .get("https://api.themoviedb.org/3/search/movie", {
+  //       params: {
+  //         api_key: "149f6f231b74bbad3f1401daa78a4cce",
+  //         query: "ritorno+al+futuro",
+  //         language: "it-IT"
+  //       }
+  //     })
+  //     .then( result => {
+  //       this.movies = result.data.results;
+  //   });
+  //
+  // }
 
 });
