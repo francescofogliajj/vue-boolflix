15671e386bf4842d971e457b0fb1e4fae1521dc3
Francesco <francescofogliajj@gmail.com>
2020-12-21 15:50:19 +0100

    Commit #5

--- js/script.js
+++ js/script.js
@@ -33,6 +33,23 @@ var app = new Vue ({
       });
     },
 
+    languageToFlag(movie) {
+
+      switch (movie.original_language) {
+        case "it":
+          return "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/1280px-Flag_of_Italy.svg.png";
+        case "en":
+          return "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1920px-Flag_of_the_United_Kingdom.svg.png";
+        case "es":
+          return "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/1280px-Flag_of_Spain.svg.png";
+        case "fr":
+          return "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/1280px-Flag_of_France.svg.png";
+        default:
+          return "https://www.artecreo.it/3681/prodotto-non-disponibile.jpg";
+      }
+
+    }
+
   },
 
   // mounted: function() {
